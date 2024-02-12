a22fd871c66a62359e15f39e18e92d9e2b04c1f2
wstgtime <denk174286@outlook.com>
2024-02-13 04:23:11 +0800

    serve static assets using express.static() middleware

--- myApp.js
+++ myApp.js
@@ -3,6 +3,9 @@ let app = express();
 
 // console.log("Hello World");
 
+const pathToAssets = __dirname + "/public";
+app.use("/public", express.static(pathToAssets));
+
 app.get("/", function(req, res) {
     const pathToIndex = __dirname + '/views/index.html';
     res.sendFile(pathToIndex);
