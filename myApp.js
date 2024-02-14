0f3a7e763cdfbccfac7da6b2c6c42654ee3b9920
wstgtime <denk174286@outlook.com>
2024-02-15 02:50:29 +0800

    get query parameter input "first" and "last" from client

--- myApp.js
+++ myApp.js
@@ -38,4 +38,8 @@ app.get("/:word/echo", function(req, res) {
     res.json({ echo: req.params.word });
 });
 
+app.get("/name", function(req, res) {
+    res.json({ name: `${req.query.first} ${req.query.last}`});
+});
+
 module.exports = app;
