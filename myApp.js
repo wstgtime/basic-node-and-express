e04e1a1f8fd40882189165ea1ebc39b6d7458a4f
wstgtime <denk174286@outlook.com>
2024-02-16 03:02:06 +0800

    retrieve and return login post data as parsed by body-parser

--- myApp.js
+++ myApp.js
@@ -45,6 +45,10 @@ app.get("/name", function(req, res) {
     res.json({ name: `${req.query.first} ${req.query.last}`});
 });
 
+app.post("/name", function(req, res) {
+    res.json({ name: `${req.body.first} ${req.body.last}`});
+});
+
 
 
 module.exports = app;
